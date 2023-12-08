72cfd38fe30f5929f4005640d234a2cdfbfe2634
Lena <olena.mitropolska.gmail.com>
2023-12-08 14:09:43 +0100

    navbar responsive all done

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function SearchInput() {
   return (
     <div>
-        <form className="flex justify-between input input-bordered input-md w-full max-w-xs">
-        <input type="text" placeholder="What are you looking for?" className='order-none rounded-3xl'/>
+        <form className="flex justify-between input input-bordered input-md w-full max-w-xs search-input-form">
+        <input type="text" placeholder="What are you looking for?" className='order-none rounded-3xl input-nav'/>
             <button type='submit' class="text-2xl"><ion-icon name="search-outline"></ion-icon></button>
         </form>
     </div>
