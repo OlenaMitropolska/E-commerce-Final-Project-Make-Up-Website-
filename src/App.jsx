533f7e358790fc020ea1bb76954aafadc45a0d1c
Lena <olena.mitropolska.gmail.com>
2023-12-08 09:26:40 +0100

    daisy UI and Tailwind installed

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div data-theme="fantasy" className="App">
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
