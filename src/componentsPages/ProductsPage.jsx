bd7de44e6fa9c6c7b673a8dead7f3a6682e2d90e
Lena <olena.mitropolska.gmail.com>
2023-12-09 23:33:34 +0100

    products display added info and created responsive design

--- src/componentsPages/ProductsPage.jsx
+++ src/componentsPages/ProductsPage.jsx
@@ -1,5 +1,6 @@
 import { useAppContext } from '../context/appContext'
 
+
 function ProductsPage() {
     const {dataRetriever} = useAppContext()
     const {products} = useAppContext()
@@ -12,10 +13,12 @@ function ProductsPage() {
 {products && products.map((product, j) => <div key={j} className='products_presentation'>
    <img src={product.image_link} alt="" />
 <div className='products_presentation_content'>
-<h3> {product.name}</h3>
+<h2> {product.name}</h2>
    <p>{product.brand}</p>
+   <p>{product.price} &euro;</p>
 </div>
-   <button className='btn'><a className="text-2xl"><ion-icon name="bag-outline"></ion-icon></a></button>
+<p className='products_presentation_additional'>*Inkl. MwSr. und zzgl.Versand</p>
+<button className='btn'><a className="text-2xl"><ion-icon name="bag-outline"></ion-icon></a></button>
      </div>)}
      </div>
      {/* end of products display */}
